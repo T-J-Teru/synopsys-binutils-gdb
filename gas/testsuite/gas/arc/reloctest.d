f7eccd756515f6f8c75c89762ff18833401ddff3
Andrew Burgess <andrew.burgess@embecosm.com>
2014-12-06 17:02:04 +0000

    gas: reloctest.d, update expected output.
    The expected output for this test does not match the expected output
    from the disassembler.  Update the output to match current disassembler
    style.
    
    gas/testsuite/ChangeLog:
    
    	* testsuite/gas/arc/reloctest.d: Update expected output.

--- gas/testsuite/gas/arc/reloctest.d
+++ gas/testsuite/gas/arc/reloctest.d
@@ -3,12 +3,14 @@
 
 .*: +file format elf32-.*arc
 
-Disassembly of section .text:
+Disassembly of section \.text:
 
 00000000 <_foo>:
-   0:	00 d8 	        0000d800     mov_s      r0,0
-Disassembly of section .data:
+   0:	00 d8                   	mov_s      r0,0
+
+Disassembly of section \.data:
 
 00000000 <_abc>:
-   0:	00 00 00 00 	00000000                
-			0: R_ARC_32	.text
+   0:	00 00 00 00             	           
+			0: R_ARC_32	\.text
+#...
